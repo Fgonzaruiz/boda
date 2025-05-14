7b68a4b06c417c7e91ab59e761f27736b8863e69
Fgonzaruiz <104510616+Fgonzaruiz@users.noreply.github.com>
2025-05-14 08:49:42 +0200

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,22 @@
+
+const countDownDate = new Date("June 12, 2026 00:00:00").getTime();
+
+const countdownfunction = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.querySelector(".countdown").innerHTML = "¡Ya llegó el gran día!";
+    }
+}, 1000);
